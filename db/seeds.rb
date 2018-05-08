a5d46ceec45a9abe8e608729b9cce2af68107afe
raphsutti <neoster9@gmail.com>
2018-05-08 11:46:08 +1000

    A2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Product.delete_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(title: 'Sevent Mobile Apps in Seven Weeks',
+  description:
+    %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question "Can we build this for ALL the devices?" with a resounding YES. This book will help you get there with a real-world introduction to seven platforms, whether you're new to mobile or an experienced developer needing to expand your options. Plus, you'll find out which cross-platform solution makes the most sense for your needs.
+      </p>},
+    image_url: '7apps.jpg',
+    price: 26.00)
+
+Product.create!(title: 'Ruby Performance Optimization',
+  description:
+    %{<p>
+        You don't have to accept slow Ruby or Rails performance. In this comprehensive guide to Ruby optimization, you'll learn how to write faster Ruby code--but that's just the beginning. See exactly what makes Ruby and Rails code slow, and how to fix it.
+      </p>},
+    image_url: 'adrpo.jpg',
+    price: 26.00)
